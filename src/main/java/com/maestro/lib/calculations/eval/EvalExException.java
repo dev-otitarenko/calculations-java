03f5e575d830193bf94281faf914b77d03686ce2
oleksii titarenko <atitarenko.eng@gmail.com>
2021-01-24 20:49:22 +0100

    eval: added new classes

---
+++ src/main/java/com/maestro/lib/calculations/eval/EvalExException.java
@@ -0,0 +1,12 @@
+package com.maestro.lib.calculations.eval;
+
+    /**
+     * The expression evaluators exception class.
+     */
+public class EvalExException extends RuntimeException {
+    private static final long serialVersionUID = 1118142866870779047L;
+
+    public EvalExException(String message) {
+        super(message);
+    }
+}
